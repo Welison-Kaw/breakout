e2a407364b2f41a231e90ab9791a8d0cc26ec289
Welison-Kaw <welisoncomw@gmail.com>
2020-02-08 12:25:32 -0300

    Create Ball.js
    Created Ball.js with Ball class

---
+++ js/Ball.js
@@ -0,0 +1,51 @@
+class Ball {
+	constructor (x, y, increment, limitX, limitY) {
+		this._x = x;
+		this._y = y;
+		this._increment = increment;
+		this._directionX = 1;
+		this._directionY = 1;
+		this._limitX = limitX;
+		this._limitY = limitY;		
+	}
+
+	get x() {
+		return this._x;
+	}
+
+	get y() {
+		return this._y;
+	}
+
+	get directionX() {
+		return this._directionX;
+	}
+
+	get directionY() {
+		return this._directionY;
+	}
+
+	changeDirectionX() {
+		this._directionX *= -1;
+	}
+
+	changeDirectionY() {
+		this._directionY *= -1;
+	}
+
+	move() {
+		if ((this._x + this._increment > this._limitX && this._directionX ==   1) ||
+			(this._x - this._increment < 0            && this._directionX == - 1)) {
+				this._directionX *= -1;
+		}
+
+		if ((this._y + this._increment > this._limitY && this._directionY ==   1) ||
+			(this._y - this._increment < 0            && this._directionY == - 1)) {
+				this._directionY *= -1;
+		}
+
+		this._x += this._increment * this._directionX;
+		this._y += this._increment * this._directionY;
+
+	}
+}
